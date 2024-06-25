d3534e3ce60533a9c19aa3e233e9921fd1c1d89a
bartlomiej.zylinski <b.zylinski.o@gmail.com>
2024-06-25 11:23:33 +0200

    Added Google Code Style

--- src/test/java/otter/jet/plaintext/PlainTextMessageReaderTest.java
+++ src/test/java/otter/jet/plaintext/PlainTextMessageReaderTest.java
@@ -1,14 +1,12 @@
 package otter.jet.plaintext;
 
-import static org.awaitility.Awaitility.await;
-
 import com.github.javafaker.Faker;
+
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
-import org.assertj.core.api.Assertions;
 import otter.jet.AbstractIntegrationTest;
 import otter.jet.JetStreamContainerInitializer;
 import otter.jet.JetStreamUtils;
@@ -21,41 +19,36 @@ import org.springframework.test.context.TestPropertySource;
 import otter.jet.store.Filters;
 import otter.jet.store.MessageStore;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
 @TestPropertySource(properties = {"read.mode=plaintext", "read.subject=plaintext"})
 class PlainTextMessageReaderTest extends AbstractIntegrationTest {
 
-  private static final LocalDateTime ignoredMessageTimestamp =
-      LocalDateTime.ofInstant(Instant.EPOCH, ZoneOffset.UTC);
-  @Autowired private MessageStore messageStore;
-  @Autowired private ReaderConfigurationProperties readerConfigurationProperties;
+  private static final LocalDateTime ignoredMessageTimestamp = LocalDateTime.ofInstant(
+      Instant.EPOCH, ZoneOffset.UTC);
+  @Autowired
+  private MessageStore messageStore;
+  @Autowired
+  private ReaderConfigurationProperties readerConfigurationProperties;
 
   @Test
   public void shouldReadMessagesSentInPlaintext() {
     // given
-    JetStreamUtils.createSubjectStream(
-        readerConfigurationProperties.getSubject(),
+    JetStreamUtils.createSubjectStream(readerConfigurationProperties.getSubject(),
         JetStreamContainerInitializer.getNatsServerUrl());
     String randomName = new Faker().name().fullName();
     byte[] data = randomName.getBytes(StandardCharsets.UTF_8);
 
     // when
-    JetStreamUtils.tryToSendMessage(
-        data,
-        readerConfigurationProperties.getSubject(),
+    JetStreamUtils.tryToSendMessage(data, readerConfigurationProperties.getSubject(),
         JetStreamContainerInitializer.getNatsServerUrl());
 
     // then
-    await()
-        .untilAsserted(
-            () ->
-                Assertions.assertThat(messageStore.filter(Filters.empty(), 0, 10))
-                    .usingRecursiveFieldByFieldElementComparator(
-                        ComparisonConfiguration.configureReadMessageComparison())
-                    .contains(
-                        new ReadMessage(
-                            readerConfigurationProperties.getSubject(),
-                            "",
-                            randomName,
-                            ignoredMessageTimestamp)));
+    await().untilAsserted(() -> assertThat(
+        messageStore.filter(Filters.empty(), 0, 10)).usingRecursiveFieldByFieldElementComparator(
+        ComparisonConfiguration.configureReadMessageComparison()).contains(
+        new ReadMessage(readerConfigurationProperties.getSubject(), "", randomName,
+            ignoredMessageTimestamp)));
   }
 }
